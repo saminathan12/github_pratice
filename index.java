d9df0a870834d0ffbc2e2a90fce5708224b4b1cd
saminathan12 <144705050+saminathan12@users.noreply.github.com>
2024-11-29 15:08:51 +0530

    fix:add java files some add function

--- index.java
+++ index.java
@@ -3,5 +3,8 @@ public class index {
 
     public static void main(String[] args) {
         System.out.println("hello");
+        int a = 10;
+        int b= 20;
+        System.out.print(a+b);
     }
 }
