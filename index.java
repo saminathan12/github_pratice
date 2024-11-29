67b2967d418e202729facef62105b278fb82bd99
saminathan12 <sn076240@gmail.com>
2024-11-29 15:49:41 +0530

    Feat:merge files

---
+++ index.java
@@ -0,0 +1,11 @@
+
+public class index {
+
+    public static void main(String[] args) {
+        System.out.println("hello");
+        System.out.println("what is your name:");
+        int a = 10;
+        int b= 20;
+        System.out.print(a+b);
+    }
+}
