57430777f1181f9f1e90e2f97fc7fc0d69b024e3
saminathan12 <sn076240@gmail.com>
2024-12-02 12:31:30 +0530

    Feat : Scanner class intro

---
+++ Scan_intro.java
@@ -0,0 +1,11 @@
+import java.lang.System;
+import java.util.Scanner;
+public class Scan_intro {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        System.out.println(a);
+        
+    }
+}
